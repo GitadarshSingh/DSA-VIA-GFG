ec1f88d3bb449ac2eb16173cb6400ebc274c73ea
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2025-06-14 12:39:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/First Occurence/first-occurence.java
@@ -0,0 +1,13 @@
+class Solution {
+    // Function to locate the occurrence of the string x in the string s.
+    int firstOccurence(String a, String b) {
+        // int m = a.length();
+        // int n = b.length();
+        
+        return a.indexOf(b);
+        
+    
+        
+        
+    }
+}
